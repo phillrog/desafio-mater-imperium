40272600e2a2214e8d6e6f8776515a8497ee9b3e
Phillipe R Souza <8622005+phillrog@users.noreply.github.com>
2026-02-10 17:47:31 -0300

    regras dos arquivos

--- src/mater-imperium-api/src/main/java/com/materimperium/backend/modules/processamento/application/validators/ArquivoValidator.java
+++ src/mater-imperium-api/src/main/java/com/materimperium/backend/modules/processamento/application/validators/ArquivoValidator.java
@@ -12,6 +12,16 @@ public class ArquivoValidator {
     public void validarCabecalho(InputStream inputStream) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
 
+        // Regra: 1ª linha deve começar com |0000|017| ou |0000|006|
+        String primeiraLinha = reader.readLine();
+        if (primeiraLinha == null || (!primeiraLinha.startsWith("|0000|017|") && !primeiraLinha.startsWith("|0000|006|"))) {
+            throw new IllegalArgumentException("Arquivo inválido: Cabeçalho da primeira linha incorreto.");
+        }
 
+        // Regra: 2ª linha deve conter exatamente |0001|0|
+        String segundaLinha = reader.readLine();
+        if (segundaLinha == null || !segundaLinha.contains("|0001|0|")) {
+            throw new IllegalArgumentException("Arquivo inválido: Segunda linha não contém o marcador esperado.");
+        }
     }
 }
